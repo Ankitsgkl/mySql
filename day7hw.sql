7667898af717f3ac9b639b32d7adf970b7dabc16
Ankitsgkl <ankitsgkl17@gmail.com>
2025-11-21 15:06:24 +0530

    small change

--- day7hw.sql
+++ day7hw.sql
@@ -1,2 +1,3 @@
+INSERT INTO performance  VALUES (1, 'Raj', 'Chennai', 88, 5, 'Fitness'),(2, 'Anu', 'Mumbai', 91, NULL, 'Diet'),(3, 'Ravi', 'Chennai', 78, 3, 'Fitness'),(4, 'Meena', 'Delhi', 82, NULL, 'Diet'),(5, 'Farah', 'Mumbai', 95, 4, 'Fitness'),(6, 'Kiran', 'Pune', 70, NULL, 'Yoga'),(7, 'Latha', 'Pune', 87, NULL, 'Fitness');
 SELECT * FROM performance WHERE score > (SELECT AVG(score) FROM performance);
 SELECT name FROM performance WHERE challenge = (SELECT challenge FROM performance WHERE name = 'Farah');
