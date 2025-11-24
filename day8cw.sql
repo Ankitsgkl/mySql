f36121c489ad7e6d79cc75e285816cf7ca2c4c81
Ankitsgkl <ankitsgkl17@gmail.com>
2025-11-24 15:10:42 +0530

    day 8 hw and cw

---
+++ day8cw.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE GroceryShop;
+CREATE TABLE porducts( product_id int(5), product_name varchar(25), price varchar(500));
+ALTER TABLE porducts ADD category varchar(25);
+INSERT INTO porducts (product_id, product_name, price, category) VALUES (1, 'Laptop', 55000, 'Electronics'), (2, 'Headphones', 1500, 'Accessories'), (3, 'Coffee Mug', 250, 'Kitchen'), (4, 'Office Chair', 7000, 'Furniture'), (5, 'Keyboard', 800, 'Electronics');
+TRUNCATE TABLE porducts;
+DROP DATABASE groceryshop;
